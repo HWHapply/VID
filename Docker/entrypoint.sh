a534a737524ff1b20bde96a78d7661a2e9e18a0d
Han Wenhao <95114275+HWHapply@users.noreply.github.com>
2024-09-27 00:12:48 +0800

    Add files via upload

---
+++ Docker/entrypoint.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# Initialize variables
+args=""
+feature_file=""
+marker_file=""
+label_file=""
+vid_file=""
+
+# Parse other arguments
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        *) args="$args $1" ;;  # Collect other arguments
+    esac
+    shift
+done
+
+# Define paths inside the container
+container_input_path="/wkdir/input/data.rds"
+container_output_dir="/wkdir/output"
+container_marker_path="/wkdir/input/markers.txt"
+container_feature_path="/wkdir/input/features.txt"
+container_label_path="/wkdir/input/labels.txt"
+container_vid_path="/wkdir/input/vid.pkl"
+
+# Check if the feature file is mounted (i.e., -v is used to mount a file)
+if [[ -f "$container_feature_path" ]]; then
+    feature_file="--feature_dir $container_feature_path"
+fi
+
+# Check if the feature file is mounted (i.e., -v is used to mount a file)
+if [[ -f "$container_marker_path" ]]; then
+    marker_file="--marker_dir $container_marker_path"
+fi
+
+# Check if the feature file is mounted (i.e., -v is used to mount a file)
+if [[ -f "$container_label_path" ]]; then
+    label_file="--label_dir $container_label_path"
+fi
+
+# Check if the feature file is mounted (i.e., -v is used to mount a file)
+if [[ -f "$container_vid_path" ]]; then
+    vid_file="--vidmodel_dir $container_vid_path"
+fi
+
+
+# Run the main script with the provided arguments
+bash /opt/VID/run_vid "$container_input_path" --output_dir "$container_output_dir"  $feature_file $args $vid_file $marker_file $label_file
+
+echo "VID process complete!"
+
