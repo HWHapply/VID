06597756dad689f0eed465f242941abb1af547b8
Han Wenhao <95114275+HWHapply@users.noreply.github.com>
2025-06-14 21:21:30 +0800

    Add files via upload

--- bin/Utils.py
+++ bin/Utils.py
@@ -339,42 +339,6 @@ class Utils_Model:
         # Close to free memory
         plt.close(fig)
 
-
-
-    def histogram(self):
-        """
-        Plotting the histogram for predicted probabilities on test and unseen datasets.
-        """
-        # Global font settings
-        plt.rcParams['font.size'] = 8
-
-        # Create subplots: 1 row x 2 columns
-        fig, axes = plt.subplots(1, 2, figsize=(8, 4), dpi=600)
-
-        datasets = [('Test Set', self.pred_proba['VID']), ('Unseen Set', self.meta_unknown['infection_probability'])]
-
-        for ax, (label, data) in zip(axes, datasets):
-            sns.histplot(data, bins=30, color='darkblue', ax=ax)
-
-            # Add threshold line if defined
-            if self.threshold is not None:
-                ax.axvline(x=self.threshold, color='red', linestyle='dashed', label=f'Threshold = {self.threshold:.2f}')
-                ax.legend(loc='upper center', fontsize=8)
-
-            # Styling
-            ax.set_xlabel("Infection Probability", fontsize = 8)
-            ax.set_ylabel("Count", fontsize = 8)
-            ax.set_title(label, fontsize = 8)
-            ax.grid(False)
-            ax.spines['top'].set_visible(False)
-            ax.spines['right'].set_visible(False)
-
-        plt.tight_layout()
-        # Save the plot
-        plt.savefig(os.path.join(self.output_dir, 'Infection_probability_histogram.png'), dpi=600, bbox_inches="tight")
-        
-        plt.close()
-
         
     def xgb_feature_importance(self):
         '''
@@ -429,210 +393,44 @@ class Utils_Model:
     
 
 
+    def cv_bar_plot(self):
+        '''
+        Visualize the cross validation score with the bar plot.
+        '''
+        best_idx = self.grid_result.best_index_
+        metrics = []
+        means = []
+        stds =  []
+        for metric in self.scoring.keys():
+            metrics.append(metric)
+            means.append(self.grid_result.cv_results_[f'mean_test_{metric}'][best_idx])
+            stds.append(self.grid_result.cv_results_[f'std_test_{metric}'][best_idx])
         
-    def evaluate_models_with_bootstrap(self, n_bootstrap=1000):
-        """
-        Evaluate multiple models using bootstrap sampling for classification metrics.
+        # Compute dynamic ylim
+        ymin = max(0, min([m - s for m, s in zip(means, stds)]) - 0.05)
+        ymax = min(1.05, max([m + s for m, s in zip(means, stds)]) + 0.05)
 
-        Args:
-            n_bootstrap: int, the number iteration for bootstrap.
-        """
-        # Define scoring functions
-        metrics = {
-            'Balanced Accuracy': balanced_accuracy_score,
-            'Precision': precision_score,
-            'Recall': recall_score,
-            'Specificity': recall_score,
-            'F1-score': f1_score,
-            'ROC_AUC': roc_auc_score,
-        }
+        # Create figure
+        plt.figure(figsize=(8, 5))
+        bars = plt.bar(metrics, means, yerr=stds, capsize=8, color='skyblue', edgecolor='black')
 
-        # Initialize structure to store bootstrapped scores
-        bootstrap_scores = {
-            metric: {model: [] for model in self.pred_proba}
-            for metric in metrics
-        }
-
-        # Perform bootstrapping
-        for _ in range(n_bootstrap):
-            # Resample with replacement until both classes are present
-            while True:
-                indices = resample(np.arange(len(self.y_test)))
-                y_true_boot = self.y_test[indices]
-                if len(np.unique(y_true_boot)) == 2:
-                    break
-
-            for model, probs in self.pred_proba.items():
-                y_prob = np.array(probs)[indices]
-                y_pred = (y_prob >= 0.5).astype(int)
-
-                for metric_name, metric_func in metrics.items():
-                    if metric_name == 'ROC_AUC': 
-                        score = metric_func(y_true_boot, y_prob)
-                    elif metric_name == 'Balanced Accuracy':
-                        score = metric_func(y_true_boot, y_pred)
-                    elif metric_name == 'Specificity':
-                        score = metric_func(y_true_boot, y_pred, pos_label = 0, zero_division = 0)
-                    else:
-                        score = metric_func(y_true_boot, y_pred, zero_division=0)
-                    bootstrap_scores[metric_name][model].append(score)
-
-        # Compute mean and CI
-        results_dict = {}
-
-        for metric_name, scores_by_model in bootstrap_scores.items():
-            metric_df = pd.DataFrame(columns=['mean_score', 'ci_lower', 'ci_upper'])
-
-            for model, scores in scores_by_model.items():
-                mean_score = np.mean(scores)
-                ci_lower = np.percentile(scores, 2.5)
-                ci_upper = np.percentile(scores, 97.5)
-
-                metric_df.loc[model] = [mean_score, ci_lower, ci_upper]
-
-            results_dict[metric_name] = metric_df
-
-        return results_dict
-
-
-    def forest_plot(self):
-        """
-        Draw the forest plot. 
-        """
-        for key in self.ci_dict.keys():
-            df = self.ci_dict[key]
-            df = df.round(2)
-            # Set global font
-            plt.rcParams['font.size'] = 8
-            
-            # Create forest plot
-            plot = EffectMeasurePlot(label=list(df.index), effect_measure=list(df['mean_score']), lcl=list(df['ci_lower']), ucl=list(df['ci_upper']))
-            plot.labels(effectmeasure='Mean', center=max(df['mean_score']))
-            plot.colors(pointshape="D", color='red')
-            
-            # Adjusted figure size
-            fig_width, fig_height = 4, 1.5
-            ci_range = df['ci_upper'].max() - df['ci_lower'].min()
-            padding = 0.1 * ci_range
-            max_value = round(df['ci_upper'].max() + 1.5 * padding, 2)
-            min_value = round(max(df['ci_lower'].min() - padding, 0), 2)
-            ax = plot.plot(figsize=(fig_width, fig_height), 
-                           t_adjuster=0.06,
-                           max_value=max_value, 
-                           min_value=min_value,
-                           text_size=8,
-                           decimal=2,
-                           size=1)
-            
-            # Title
-            plt.title(f"{key}", loc="center", x=-0.82, y=1.045, fontsize=10, fontweight = 'bold')
-            plt.suptitle("Model", x=0.08, y=0.913, fontsize=8)
-            
-            # Axis styling
-            ax.set_xlabel("Lower Performance                        Higher Performance", fontsize=8)
-            ax.spines['top'].set_visible(False)
-            ax.spines['right'].set_visible(False)
-            ax.spines['left'].set_visible(False)
-            ax.spines['bottom'].set_visible(True)
-            
-            # Save the histogram of predicted probability
-            plt.savefig(os.path.join(self.output_dir, f'{key}_forest.png'), dpi=600, bbox_inches="tight")  
-
-            # Close the plot to free up memory
-            plt.close()
-
-
-
-    def combine_images_pillow(self):
-        """
-        Combine PNG images into a grid using PIL with added space between images.
-        """
-        # set the layout parameters
-        grid_shape=(3, 2)
-        padding=250
-        
-        # load image and get image attributes
-        image_files = ['Precision_forest.png',
-                       'Balanced Accuracy_forest.png',
-                       'Recall_forest.png',
-                       'F1-score_forest.png',
-                       'Specificity_forest.png',
-                       'ROC_AUC_forest.png']
-        images = [Image.open(os.path.join(self.output_dir, f)) for f in image_files]
-        rows, cols = grid_shape
-        width, height = images[0].size
-
-        # Create a blank canvas with extra space for padding between images
-        combined_img = Image.new(
-            'RGB', 
-            ((cols * width) + (cols - 1) * padding, (rows * height) + (rows - 1) * padding), 
-            (255, 255, 255)
-        )
-
-        # paste the image on the canvas
-        for idx, img in enumerate(images):
-            if idx >= rows * cols:
-                break
-            x_offset = (idx % cols) * (width + padding)
-            y_offset = (idx // cols) * (height + padding)
-            combined_img.paste(img, (x_offset, y_offset))
-
-        # save the iamge
-        combined_img.save(os.path.join(self.output_dir, 'Forest_plot_test.png'), dpi=(600, 600))
-        
-        # remove the separate images
-        for image in image_files:
-            os.remove(os.path.join(self.output_dir, image))
-            
-            
-    def calibration_plot(self):
-        """
-        Draw the calibration file for all models
-        """
-        plt.rcParams['font.size'] = 8
-        fig = plt.figure(figsize=(12, 10))
-        gs = GridSpec(5, 2)
-        colors = plt.get_cmap("Dark2")
-
-        ax_calibration_curve = fig.add_subplot(gs[:2, :2])
-        calibration_displays = {}
-        for i, (clf, name) in enumerate(self.clf_list):
-            if name == 'VID':
-                X_test = self.X_test_norm[self.features]
-            else:
-                X_test = self.X_test_norm
-            display = CalibrationDisplay.from_estimator(
-                clf,
-                X_test,
-                self.y_test,
-                n_bins=10,
-                name=name,
-                ax=ax_calibration_curve,
-                color=colors(i),
+        # Add text labels: mean (std) above error bar
+        for bar, mean, std in zip(bars, means, stds):
+            yval = bar.get_height() + std + 0.005  # position above error bar
+            plt.text(
+                bar.get_x() + bar.get_width() / 2,
+                yval,
+                f'{mean:.3f} (±{std:.3f})',
+                ha='center',
+                va='bottom',
+                fontsize=10
             )
-            calibration_displays[name] = display
 
-        ax_calibration_curve.grid()
-        ax_calibration_curve.set_title("Calibration Curves")
-
-        # Add histogram
-        grid_positions = [(2, 0), (2, 1), (3, 0), (3, 1), (4, 0), (4, 1)]
-        for i, (_, name) in enumerate(self.clf_list):
-            row, col = grid_positions[i]
-            ax = fig.add_subplot(gs[row, col])
-
-            ax.hist(
-                calibration_displays[name].y_prob,
-                range=(0, 1),
-                bins=10,
-                label=name,
-                color=colors(i),
-            )
-            ax.set(title=name, xlabel="Mean predicted probability", ylabel="Count")
-
+        # Titles and labels
+        plt.ylim(ymin, ymax)
+        plt.title('Cross-Validation Scores (Mean ± Std)', fontsize=12, fontweight='bold')
+        plt.ylabel('Scores', fontsize=10, fontweight='bold')
+        plt.grid(axis='y', linestyle='--', alpha=0.7)
         plt.tight_layout()
-        # Save the histogram of predicted probability
-        plt.savefig(os.path.join(self.output_dir, f'Calibration_plot_test.png'), dpi=600, bbox_inches="tight")  
-
-        # Close the plot to free up memory
+        plt.savefig(os.path.join(self.output_dir, 'CV_bar.png'), dpi=600, bbox_inches="tight")
         plt.close()
